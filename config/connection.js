c612a7cd4b76149a420199beb48e5f0e95fd5aca
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-05-07 23:10:16 +1000

    added connection file

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const { connect, connection } = require('mongoose');
+
+connect('mongodb+srv://lauraC:Laura123@cluster0.wnxdact.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection;
