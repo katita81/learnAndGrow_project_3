cc6c9a4f3e09a6d8ccc1734ec33683f6a33a6f7d
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-05-08 22:57:47 +1000

    deleted folders

--- config/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lauraC:Laura123@cluster0.wnxdact.mongodb.net/?retryWrites=true&w=majority');
-
-module.exports = mongoose.connection;
