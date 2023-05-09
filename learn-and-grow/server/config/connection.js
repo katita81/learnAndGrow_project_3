865c9b278518635f387b14e87cd874db7db77dd4
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-05-09 17:40:57 +1000

    try to fix bug preventing user to sign up or login

--- learn-and-grow/server/config/connection.js
+++ learn-and-grow/server/config/connection.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lauraC:Laura123@cluster0.wnxdact.mongodb.net/?retryWrites=true&w=majority');
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test')
 
-module.exports = mongoose.connection; //mongodb://127.0.0.1:27017/programming-thoughts
-//1.144.107.54/32 
+module.exports = mongoose.connection; 
