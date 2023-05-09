7c259bcf4752254b37cbbbaf0d3b8f51251a1fdf
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-05-09 19:41:38 +1000

    pushing to Heroku

--- learn-and-grow/server/config/connection.js
+++ learn-and-grow/server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lauraC:Laura123@cluster0.wnxdact.mongodb.net/?retryWrites=true&w=majority');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test');
 
 module.exports = mongoose.connection; 
